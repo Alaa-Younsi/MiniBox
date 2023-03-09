48f2b19fd424c2430a36299366f223c5ae48ca42
Philip Marlowe <103404455+Alaa-Younsi@users.noreply.github.com>
2023-03-09 14:35:05 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,230 @@
+$(document).ready(function(){
+
+  $("#Home").click(function(){
+      $("#Who").fadeIn(500);
+    });
+});
+
+$(document).ready(function(){
+
+  $("#Info").click(function(){
+      $("#Who").fadeOut(250);
+    });
+});
+
+$(document).ready(function(){
+
+  $("#Info").click(function(){
+      $("#Info-text").fadeIn(250);
+    });
+});
+
+$(document).ready(function(){
+
+  $("#Home").click(function(){
+      $("#Info-text").fadeOut(250);
+    });
+});
+
+$(document).ready(function(){
+
+  $("#Projects").click(function(){
+      $("#Projects-text").fadeIn(250);
+    });
+});
+
+$(document).ready(function(){
+
+  $("#Projects").click(function(){
+      $("#Who").fadeOut(250);
+    });
+});
+
+$(document).ready(function(){
+
+  $("#Home").click(function(){
+      $("#Projects-text").fadeOut(250);
+    });
+});
+
+$(document).ready(function(){
+
+  $("#Info").click(function(){
+      $("#Projects-text").fadeOut(250);
+    });
+});
+
+$(document).ready(function(){
+
+  $("#Projects").click(function(){
+      $("#Info-text").fadeOut(250);
+    });
+});
+
+$(document).ready(function(){
+
+  $("#Contact").click(function(){
+      $(".contact-text").fadeIn(250);
+    });
+});
+
+$(document).ready(function(){
+
+  $("#Contact").click(function(){
+      $("#Who").fadeOut(250);
+    });
+});
+
+$(document).ready(function(){
+
+  $("#Home").click(function(){
+      $(".contact-text").fadeOut(250);
+    });
+});
+
+$(document).ready(function(){
+
+  $("#Info").click(function(){
+      $(".contact-text").fadeOut(250);
+    });
+});
+
+$(document).ready(function(){
+
+  $("#Projects").click(function(){
+      $(".contact-text").fadeOut(250);
+    });
+});
+
+$(document).ready(function(){
+
+  $("#Contact").click(function(){
+      $("#Info-text").fadeOut(250);
+    });
+});
+
+$(document).ready(function(){
+
+  $("#Contact").click(function(){
+      $("#Projects-text").fadeOut(250);
+    });
+});
+
+$(document).ready(function(){
+
+  $("#Button-off").click(function(){
+      $("#Page").slideUp(2500);
+    });
+});
+
+$(document).ready(function(){
+
+  $("#Button-off").click(function(){
+      $("#Mini-page2").fadeOut(500);
+    });
+});
+
+$(document).ready(function(){
+
+  $("#Button-off").click(function(){
+      $("#More").fadeOut(500);
+    });
+});
+
+$(document).ready(function(){
+
+  $("#Button-off").click(function(){
+      $("#More1").fadeOut(500);
+    });
+});
+
+$(document).ready(function(){
+
+  $("#Button-off").click(function(){
+      $("#Tv").fadeOut(500);
+    });
+});
+
+$(document).ready(function(){
+
+  $("#Button-off").click(function(){
+      $("#Remote").fadeOut(500);
+    });
+});
+
+$(document).ready(function(){
+
+  $("#Button-off").click(function(){
+      $("#c").fadeIn(8000);
+    });
+});
+
+$(document).ready(function(){
+
+  $("#Button-off").click(function(){
+      $("#Page2-button").fadeIn(8000);
+    });
+});
+
+$(document).ready(function(){
+
+  $("#Button-off").click(function(){
+      $("#Page2-title").fadeIn(8000);
+    });
+});
+
+$(document).ready(function(){
+
+  $("#Page2-button").click(function(){
+      $("#Page").slideDown(2500);
+    });
+});
+
+$(document).ready(function(){
+
+  $("#Page2-button").click(function(){
+      $("#c").fadeOut(500);
+    });
+});
+
+$(document).ready(function(){
+
+  $("#Page2-button").click(function(){
+      $("#Page2-title").fadeOut(500);
+    });
+});
+
+$(document).ready(function(){
+
+  $("#Page2-button").click(function(){
+      $("#Page2-button").fadeOut(500);
+    });
+});
+
+$(document).ready(function(){
+
+  $("#Page2-button").click(function(){
+      $("#Mini-page2").fadeIn(3000);
+    });
+});
+
+$(document).ready(function(){
+
+  $("#Page2-button").click(function(){
+      $("#Tv").fadeIn(2500);
+    });
+});
+
+$(document).ready(function(){
+
+  $("#Page2-button").click(function(){
+      $("#Remote").fadeIn(2500);
+    });
+});
+
+$(document).ready(function(){
+
+  $("#Page2-button").click(function(){
+      $("#More").fadeIn(2500);
+    });
+});
